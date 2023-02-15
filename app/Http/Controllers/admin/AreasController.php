101f576f02b151441d8c8c20934c73431b81b7eb
thanhtd2000 <thanhhandgun2@gmail.com>
2023-02-15 21:03:22 +0700

    Update AreasController.php

--- app/Http/Controllers/admin/AreasController.php
+++ app/Http/Controllers/admin/AreasController.php
@@ -6,6 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AreasController extends Controller
-{
-    //
+{   
+     //Get Area Controller
+     public function index(){
+
+     }
+     //Create Area  
+     public function create(){
+ 
+     }
+     //Delete Area
+     public function delete(){
+ 
+     }
+     //update Area
+     public function update(){
+        
+     }
 }
