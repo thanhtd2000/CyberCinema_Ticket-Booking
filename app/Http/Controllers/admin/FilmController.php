5b532fe312e2d631a99c5cbf014b85799689ea4e
thanhtd2000 <thanhhandgun2@gmail.com>
2023-02-15 09:37:50 +0700

    router

---
+++ app/Http/Controllers/admin/FilmController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FilmController extends Controller
+{
+    //
+
+    //Get film
+    public function index(){
+    }
+    //Create Film  
+    public function create(){
+
+    }
+    //Delete Film
+    public function delete(){
+
+    }
+    //update Film
+    public function update(){}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,61 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::prefix('film')->group(function () {
+        Route::get('index',function(){
+            return view('admin.film.index');
+        });
+        Route::post('add',function(){
+            return view('admin.film.AddFilm');
+        });
+        Route::delete('delete/{id}',function(){
+            return view('ShowFilm');
+        });
+        
+        Route::put('update/{id}',function(){
+            return view('Updatefilm');
+        });
+        Route::get('edit',function(){
+            return view('ShowFilm');
+        });
+    });
+    Route::prefix('Areas')->group(function () {
+        Route::get('index',function(){
+            return view('admin.area.ShowAreas');
+        });
+        Route::post('add',function(){
+            return view('admin.area.AddArea');
+        });
+        Route::delete('delete/{id}',function(){
+            return view('admin.area.ShowAreas');
+        });
+        
+        Route::put('update/{id}',function(){
+            return view('admin.area.UpdateArea');
+        });
+        Route::get('edit',function(){
+            return view('admin.area.ShowAreas');
+        });
+    });
+    Route::prefix('cinema')->group(function() {
+        Route::get('index',function(){
+            return view('admin.cinema.index');
+        });
+        Route::post('add',function(){
+            return view('admin.cinema.add');
+        });
+        Route::delete('delete/{id}',function(){
+            return view('admin.cinema.index');
+        });
+        Route::get('edit/{id}',function(){
+            return view('admin.cinema.edit');
+        });
+        Route::put('update',function(){
+            return view('admin.cinema.index');
+        });
+    });
 });
