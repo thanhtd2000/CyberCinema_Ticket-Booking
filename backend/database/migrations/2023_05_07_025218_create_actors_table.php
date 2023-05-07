6e89be637e809d41f24eb9ea1b811e1ecfa4b577
HuyHoang <hoangnh@zigexn.vn>
2023-05-07 10:40:39 +0700

    migration for db

--- backend/database/migrations/2023_05_07_025218_create_actors_table.php
+++ backend/database/migrations/2023_05_07_025218_create_actors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateActorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('actors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('image');
-            $table->rememberToken();
+            $table->date('birthday');
+            $table->string('nationality');
+            $table->tinyInteger('gender');
+
+            
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('actors');
     }
 }
