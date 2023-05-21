9076fe727e8f38b821f43fb62ec5a2936a3755d7
trungdinh96 <109255230+trungdinh96@users.noreply.github.com>
2023-05-21 21:54:04 +0700

    trungdq uppdate

--- backend/app/Http/Controllers/Admin/SeatRowController.php
+++ backend/app/Http/Controllers/Admin/SeatRowController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Seat_Row;
 use Illuminate\Http\Request;
 
 class SeatRowController extends Controller
 {
-    //
+   
+
 }
