3f11c407dbc36119ac16a716cfe3f59fef4dce03
trungdinh96 <109255230+trungdinh96@users.noreply.github.com>
2023-06-05 22:43:51 +0700

    trungdq

--- backend/app/Http/Controllers/Admin/RoomController.php
+++ backend/app/Http/Controllers/Admin/RoomController.php
@@ -14,78 +14,87 @@ class RoomController extends Controller
     public $room;
     public $seat;
     public $seatType;
- 
-    public function __construct(Room $room, Seat $seat , SeatType $seatType)
+
+    public function __construct(Room $room, Seat $seat, SeatType $seatType)
     {
-        $this->room =$room;
-        $this->seat =$seat;
-        $this->seatType =$seatType;
- 
+        $this->room = $room;
+        $this->seat = $seat;
+        $this->seatType = $seatType;
     }
 
     public function index()
     {
         $rooms = $this->room->paginate(5);
-        return view('Admin/Room/list',compact('rooms'));
+        return view('Admin/Room/list', compact('rooms'));
     }
 
     public function create()
     {
-       
+
         $seatTypes = $this->seatType->get();
-       
-        return view('Admin/Room/create',compact('seatTypes'));
+
+        return view('Admin/Room/create', compact('seatTypes'));
     }
-   public function store(RoomRequest $roomRequest){
-        $data = 
-        [
-            'name' => $roomRequest->name,
-            'row' => $roomRequest->row,
-            'column' => $roomRequest->column
-        ];
-
-        $room= $this->room->create($data);
-        $alphabet = range('A', 'Z');
-        $num_of_elements = $room->row;
-        $elements = array_slice($alphabet, 0, $num_of_elements);
-        foreach($elements as $element){
-           for($i=1 ;$i <= $room->column;$i++){
-            $dataSeat = [
-                'name' => $element.$i,
-                'type_id'=> 2 ,
-                'room_id'=> $room->id
+    public function store(RoomRequest $roomRequest)
+    {
+        $data =
+            [
+                'name' => $roomRequest->name,
+                'row' => $roomRequest->row,
+                'column' => $roomRequest->column
             ];
-            $this->seat->create($dataSeat);
-           }
-        }
-        return redirect()->route('admin.room')->with('message','Thêm thành công!');
-   }
 
-   public function edit($id)
-   {
-        $seatType = $this->seatType->get();
-        $seats = $this ->seat->where('room_id', $id)->get();
-        $room = $this -> room ->find($id);
+        $room = $this->room->create($data);
         $alphabet = range('A', 'Z');
         $num_of_elements = $room->row;
         $elements = array_slice($alphabet, 0, $num_of_elements);
-        
-        return view('Admin/Room/edit',compact('room','seatType','seats','elements'));
-   }
-   public function update(RoomRequest $roomRequest, $id)
-   {
-    $data = 
-    [
-        'name' => $roomRequest->name,
-        
-    ];
-    $this->room->find($id)->update($data);
-    return redirect()->route('admin.room')->with('message','Sửa thành công!');
+        foreach ($elements as $element) {
+            for ($i = 1; $i <= $room->column; $i++) {
+                $dataSeat = [
+                    'name' => $element . $i,
+                    'type_id' => 2,
+                    'room_id' => $room->id
+                ];
+                $this->seat->create($dataSeat);
+            }
+        }
+        return redirect()->route('admin.room')->with('message', 'Thêm thành công!');
+    }
 
-   }
+    public function edit($id)
+    {
+        $seatType = $this->seatType->get();
+        $seats = $this->seat->where('room_id', $id)->get();
+        $room = $this->room->find($id);
+        $alphabet = range('A', 'Z');
+        $num_of_elements = $room->row;
+        $elements = array_slice($alphabet, 0, $num_of_elements);
 
-   public function deleteSoft($id)
-   {
-    
-   }
+        return view('Admin/Room/edit', compact('room', 'seatType', 'seats', 'elements'));
+    }
+    public function update(Request $request,$id)
+    {
+        $this->validate(
+            $request,
+            [
+                'roomname' => 'required'
+            ],
+            [
+                'roomname.required' => 'Không được bỏ trông tên phòng!',
+               
+            ]
+        );
+        $data =
+            [
+                'name' => $request->roomname,
+
+            ];
+
+        $this->room->find($id)->update($data);
+        return redirect()->route('admin.room')->with('message', 'Sửa thành công!');
+    }
+
+    public function deleteSoft($id)
+    {
+    }
 }
