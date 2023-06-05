9477bc1a16131df13414331e56389615b014ae28
HuyHoang <hoangnh@zigexn.vn>
2023-06-05 23:12:20 +0700

    update vietnamese language

--- backend/resources/views/Admin/categories/create.blade.php
+++ backend/resources/views/Admin/categories/create.blade.php
@@ -5,13 +5,13 @@
     <form method="POST" action="{{ route('admin.category.store') }}" class="container">
         @csrf
         <div class="mb-3">
-            <label class="form-label">Category Name</label>
+            <label class="form-label">Tên danh mục</label>
             <input type="text" name="name" class="form-control" value="{{ old('name') }}">
 
             @error('name')
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
             @enderror
         </div>
-        <button type="submit" class="btn btn-outline-primary">Submit</button>
+        <button type="submit" class="btn btn-outline-primary">Thêm</button>
     </form>
 @endsection
