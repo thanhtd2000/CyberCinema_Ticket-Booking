be8e2ea72b2dbce1a47fb788c4ebe5253f179688
thanhtd2000 <thanhhandgun2@gmail.com>
2023-06-25 22:07:22 +0700

    update seats api

--- backend/app/Http/Controllers/Api/SeatController.php
+++ backend/app/Http/Controllers/Api/SeatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Seat;
 use Illuminate\Http\Request;
 use App\Models\OrderSchedule;
 use Illuminate\Support\Facades\DB;
@@ -27,19 +28,22 @@ class SeatController extends Controller
                 'user_id' => $user->id,
                 'status' => 1
             ]);
-            return response()->json($data, 200);
+            $data1 = DB::table('seats')->leftJoin('seat_types', 'seat_types.id', '=', 'seats.type_id')
+                ->leftJoin('order_schedule', 'order_schedule.seat_id', '=', 'seats.id')
+                ->where('seats.id', $data->seat_id)
+                ->select('seats.id', 'seats.name', 'seat_types.price', 'order_schedule.status')->get();
+            return response()->json(SeatResource::collection($data1), 200);
         } else {
-            $data = OrderSchedule::where('seat_id', $request->id)
+            $data1 =  OrderSchedule::where('seat_id', $request->id)
                 ->where('schedule_id', $request->schedule_id)
                 ->where('user_id', $user->id)
-                ->update([
-                    'status' => DB::raw('IF(status = 0, 1, 0)')
-                ]);
-            if ($data == 0) {
+                ->where('status', 1)
+                ->delete();
+            if (empty($data1)) {
                 return response()->json(['message' => 'Ghế đã bị đặt', 'status_code' => 404], 404);
             }
-            $data = OrderSchedule::where('seat_id', $request->id)->where('schedule_id', $request->schedule_id)->first();
-            return response()->json($data, 200);
+
+            return response()->json(['message' => 'Đổi trạng thái thành công', 'status_code' => 202], 202);
         }
         // $seats = DB::table('seats')
         //     ->leftJoin('order_schedule', function ($join) use ($data) {
