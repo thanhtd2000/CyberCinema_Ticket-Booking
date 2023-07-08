3491901868a0e32fb7eb83b2cfc6a3503c9131b1
truong210120 <truongnguyen210120@gmail.com>
2023-07-08 20:54:33 +0700

    update code use

---
+++ fontend/src/pages/user/history/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '@/components/Layouts'
+import HistoryScreen from '@/components/Screens/History'
+import React from 'react'
+
+function index() {
+  return (
+    <Layout>
+      <HistoryScreen />
+    </Layout>
+  )
+}
+
+export default index
