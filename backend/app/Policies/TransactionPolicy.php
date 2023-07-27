0fbb34a451783034b5e95a0a5f86dc761d533061
trungdinh96 <109255230+trungdinh96@users.noreply.github.com>
2023-07-27 15:59:22 +0700

    trungdq

---
+++ backend/app/Policies/TransactionPolicy.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Transaction;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TransactionPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user)
+    {
+        $role =   Role::where('key', 'danh-sach-giao-dich')->where('check', 1)->first();
+        return $user->role == 0 || $user->role == ($role == null ? '' : 3);
+
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Transaction $transaction)
+    {
+        //
+    }
+}
