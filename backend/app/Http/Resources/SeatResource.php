19a7bc6959d9ed81faacdb07190fa63e4dcb424a
thanhtd2000 <thanhhandgun2@gmail.com>
2023-08-12 21:28:37 +0700

    Update SeatResource.php

--- backend/app/Http/Resources/SeatResource.php
+++ backend/app/Http/Resources/SeatResource.php
@@ -14,6 +14,11 @@ class SeatResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => (int)$this->id,
+            'name' => $this->name,
+            'price' => (int)$this->price,
+            'status' => (int)$this->status,
+        ];
     }
 }
